fc5e77deec08b3f76f0a1c65af8dd969e9696a4f
Jannik Wienecke <40333333+jannikwienecke@users.noreply.github.com>
2024-08-12 15:01:59 +0200

    fix end to end test

--- apps/fastapp-e2e/src/example.spec.ts
+++ apps/fastapp-e2e/src/example.spec.ts
@@ -2,5 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('/');
-  await expect(page.getByText('vite')).toBeVisible();
+  await expect(page.getByText('Home')).toBeVisible();
+  await expect(page.getByText('create userinterface')).toBeVisible();
 });
--- packages/convex/convex/query.ts
+++ packages/convex/convex/query.ts
@@ -1,4 +1,4 @@
-import { mutation, query } from './_generated/server';
+import { query } from './_generated/server';
 
 export const create = query({
   handler: async (ctx) => {
