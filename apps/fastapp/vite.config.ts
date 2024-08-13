4a8c63a347d55caf7b35fd2cdce3d8daa13c5240
Jannik Wienecke <40333333+jannikwienecke@users.noreply.github.com>
2024-08-13 18:27:46 +0200

    disable vite plugin

--- apps/fastapp/vite.config.ts
+++ apps/fastapp/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     host: 'localhost',
   },
 
-  plugins: [react(), nxViteTsPaths(), TanStackRouterVite()],
+  plugins: [react(), nxViteTsPaths()],
 
   // Uncomment this if you are using workers.
   // worker: {
--- libs/core/src/lib/types/base.types.ts
+++ libs/core/src/lib/types/base.types.ts
@@ -5,14 +5,6 @@ const documentBaseSchema = z.object({
   id: z.string(),
 });
 
-// const documentBaseSchema2 = z.object({
-//   id: z.object({
-//     type: z.string(),
-//     // optional or required
-//     optional: z.literal('required'),
-//   }),
-// });
-
 export type FieldType = 'String' | 'Number' | 'Boolean' | 'Date';
 
 export type RecordType<T = Record<string, unknown>> = T;
@@ -52,14 +44,6 @@ export type GlobalConfig = {
   //   };
   // };
 };
-
-type FieldSchemaType = Record<string, z.ZodTypeAny>;
-
-type TableSchemaType = {
-  schema: z.ZodTypeAny;
-};
-
-export type SchemaType = Record<string, TableSchemaType>;
 
 export type DataModel<TableNames extends string = any> = {
   tables: {
