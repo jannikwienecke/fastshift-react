900f906986c5d745d91b4e6ed2ed5778e74b49be
Jannik Wienecke <40333333+jannikwienecke@users.noreply.github.com>
2024-08-19 11:47:36 +0200

    clean up nextjs app

--- apps/nextjs/app/page.tsx
+++ apps/nextjs/app/page.tsx
@@ -1,15 +1,7 @@
-import { llinfo } from '@apps-next/core';
 import Link from 'next/link';
-import { prisma } from '../db';
 export const dynamic = 'force-dynamic';
 
-// const config = generateConfigFrom('prisma', Prisma.dmmf.datamodel);
-
 export default async function Index() {
-  const users = await prisma.user.findMany({});
-  const posts = await prisma.post.findMany({});
-
-  llinfo('Index Page', { users, posts });
   return (
     <div className="p-2">
       <div className="flex gap-2 text-lg border-b">
--- apps/nextjs/db.ts
+++ apps/nextjs/db.ts
@@ -11,18 +11,7 @@ export const singleton = <Value>(
 };
 
 const prisma = singleton('prisma', () => {
-  if (process.env.NODE_ENV !== 'production') {
-    return new PrismaClient();
-  } else {
-    // const turso = createClient({
-    //   url: process.env.TURSO_DATABASE_URL ?? '',
-    //   authToken: process.env.TURSO_AUTH_TOKEN,
-    // });
-    // const adapter = new PrismaLibSQL(turso);
-    // return new PrismaClient({ adapter });
-    // throw new Error('Not implemented');
-    return new PrismaClient();
-  }
+  return new PrismaClient();
 });
 
 prisma.$connect();
