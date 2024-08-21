9ae9f8dc06d6e731ff5ce9afeaf28c4b3b7da312
Jannik Wienecke <40333333+jannikwienecke@users.noreply.github.com>
2024-08-21 16:06:05 +0200

    form add convex

--- apps/fastapp/src/routes/fastApp.tasks.tsx
+++ apps/fastapp/src/routes/fastApp.tasks.tsx
@@ -1,76 +1,62 @@
-import { useStoreValue } from '@apps-next/core';
+import { useStoreDispatch, useStoreValue } from '@apps-next/core';
 import { createView, QueryInput, useMutation } from '@apps-next/react';
-import { Form, List, ShineBorder } from '@apps-next/ui';
+import { Form, List } from '@apps-next/ui';
 import { createFileRoute, Outlet } from '@tanstack/react-router';
 import React from 'react';
 
-export const Route = createFileRoute('/fastApp/tasks')({
-  component: createView(
-    'tasks',
+export const TasksView = createView(
+  'tasks',
 
-    {
-      displayField: {
-        field: 'name',
+  {
+    displayField: {
+      field: 'name',
 
-        // CELL UNSUPPORTED TYPE FOR CONVEX
-        // CANNOT SEND TO THE SEVER -> NEED TO CLEAN CLIENT ONLY CONFIG
-        //  cell: (value) => <div>{value.name}</div>
-      },
+      // CELL UNSUPPORTED TYPE FOR CONVEX
+      // CANNOT SEND TO THE SEVER -> NEED TO CLEAN CLIENT ONLY CONFIG
+      //  cell: (value) => <div>{value.name}</div>
     },
-    ({ data, useList, useForm }) => {
-      const getListProps = useList();
-      const getFormProps = useForm();
+  },
+  ({ data, useList, useForm }) => {
+    const getListProps = useList();
+    const getFormProps = useForm();
 
-      const { mutate } = useMutation();
-      const [isAdd, setIsAdd] = React.useState(false);
+    const { edit } = useStoreValue();
 
-      const { edit } = useStoreValue();
+    const dispatch = useStoreDispatch();
 
-      const ScreenControl = () => {
-        return (
-          <div className="w-full pb-6 flex justify-end">
-            <button
-              onClick={() => {
-                mutate({
-                  mutation: {
-                    type: 'CREATE_RECORD',
-                    record: {
-                      label: 'test',
-                      description: 'test',
-                      ownerId: 'jn7199c7wsfqz3ne782ggagsf16ywr3z',
-                      categoryId: 'jh743ap41j1q4vv2tgbx4tag656ywhaq',
-                      dueDate: new Date().getTime(),
-                    },
-                  },
-                });
-              }}
-              className="border border-black p-1 px-4 rounded-md"
-            >
-              Add
-            </button>
-          </div>
-        );
-      };
-
+    const ScreenControl = () => {
       return (
-        <div className="p-2 flex gap-2 grow overflow-scroll">
-          <div className="flex flex-col w-full ">
-            <ScreenControl />
-            <ShineBorder color={['#A07CFE', '#FE8FB5', '#FFBE7B']}>
-              <button className="w-24 h-14">Test</button>
-            </ShineBorder>
-            <h1>tasks</h1>
-            <QueryInput />
-
-            {edit.isEditing ? <Form {...getFormProps()} /> : null}
-
-            <List {...getListProps()} />
-          </div>
-          <hr />
-
-          <Outlet />
+        <div className="w-full pb-6 flex justify-end">
+          <button
+            onClick={() => dispatch({ type: 'ADD_NEW_RECORD' })}
+            className="border border-black p-1 px-4 rounded-md"
+          >
+            Add
+          </button>
         </div>
       );
-    }
-  ),
+    };
+
+    return (
+      <div className="p-2 flex gap-2 grow overflow-scroll">
+        <div className="flex flex-col w-full ">
+          <ScreenControl />
+
+          <h1>tasks</h1>
+          <QueryInput />
+
+          {edit.isEditing ? <Form {...getFormProps()} /> : null}
+
+          <List {...getListProps()} />
+        </div>
+        <hr />
+
+        <Outlet />
+      </div>
+    );
+  }
+);
+
+export const Route = createFileRoute('/fastApp/tasks')({
+  component: () => <TasksView />,
 });
--- libs/convex-adapter-app/src/lib/use-convex-mutation.ts
+++ libs/convex-adapter-app/src/lib/use-convex-mutation.ts
@@ -1,8 +1,9 @@
-import { invarant, MutationReturnType } from '@apps-next/core';
+import { invarant, MutationProps, MutationReturnType } from '@apps-next/core';
 import { useConvexMutation as useConvexMutationConvex } from '@convex-dev/react-query';
 import { useMutation } from '@tanstack/react-query';
 
 import { useConvexApi } from './_internal/useConvexApi';
+import React from 'react';
 
 export const useConvexMutation = (): MutationReturnType => {
   const api = useConvexApi();
@@ -13,9 +14,22 @@ export const useConvexMutation = (): MutationReturnType => {
     mutationFn: useConvexMutationConvex(api.viewMutation),
   });
 
+  const mutateHandler = React.useCallback(
+    async (props: MutationProps) => {
+      return mutateAsync({
+        ...props,
+        mutation: {
+          ...props.mutation,
+          handler: undefined,
+        },
+      });
+    },
+    [mutateAsync]
+  );
+
   return {
     mutate,
-    mutateAsync,
+    mutateAsync: mutateHandler,
     isPending,
   };
 };
