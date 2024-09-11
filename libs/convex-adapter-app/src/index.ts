b629c17372b051a9e85494e2a265c0ff11ae287f
Jannik Wienecke <40333333+jannikwienecke@users.noreply.github.com>
2024-09-11 12:36:23 +0200

    clean up

--- libs/convex-adapter-app/src/index.ts
+++ libs/convex-adapter-app/src/index.ts
@@ -2,12 +2,10 @@ export * from './lib/convex-view-loader';
 export * from './lib/view-mutation';
 
 export * from './lib/convex-provider';
-export * from './lib/convex-view-config';
+export * from './lib/convex-make-views';
 export * from './lib/convex-searchable-fields';
-export * from './lib/convex-view-fields';
 export * from './lib/_internal/types.convex';
 export * from './lib/convex-create-config-from-schema';
-export * from './lib/convex-create-view-config';
 export * from './lib/_internal/convex-context';
 export * from './lib/convex-normalize-schema';
 export * from './lib/make-view-loader';
